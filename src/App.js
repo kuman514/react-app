ed48f40fd47f62315852edfdeba30353f6bf4e64
kumankoishi.py <7iggif@naver.com>
2021-01-20 20:54:02 +0900

    Practicing adding event function and preventing reload when event function ends

--- src/App.js
+++ src/App.js
@@ -8,7 +8,9 @@ class App extends Component {
   constructor (props) {
     super(props)
     this.state = {
+      mode: 'read',
       subject: {title: 'WEB', sub: 'World wide web'},
+      welcome: {title: 'Welcome', desc: 'Hello, React!'},
       contents: [
         {id: 1, title: 'HTML', desc: 'HTML is for information'},
         {id: 2, title: 'CSS', desc: 'CSS is for design'},
@@ -17,16 +19,30 @@ class App extends Component {
     }
   }
   render () {
+    let _title, _desc = null
+    if (this.state.mode === 'welcome') {
+      _title = this.state.welcome.title
+      _desc = this.state.welcome.desc
+    } else if (this.state.mode === 'read') {
+      _title = this.state.contents[0].title
+      _desc = this.state.contents[0].desc
+    }
     return (
       <div className="App">
-        <Subject
+        {/*<Subject
           title={this.state.subject.title}
           sub={this.state.subject.sub}>
-        </Subject>
+        </Subject>*/}
+        <header>
+          <h1><a href="/" onClick={function (e) {
+            console.log(e)
+            e.preventDefault() // Prevent reload when event function ends
+          }}>{this.state.subject.title}</a></h1>
+          {this.state.subject.sub}
+        </header>
         <Subject title="React" sub="For UI"></Subject>
         <TOC data={this.state.contents}></TOC>
-        <Content title="HTML" dsc="HTML is HyperText Markup Language."></Content>
-        <Content title="Yasuo" dsc="Yasuo is science."></Content>
+        <Content title={_title} desc={_desc}></Content>
       </div>
     )
   }

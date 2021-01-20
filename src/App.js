3fb77dff774ebaf056190fbd00be09292ef5f9b0
kumankoishi.py <7iggif@naver.com>
2021-01-20 16:54:00 +0900

    Practicing State

--- src/App.js
+++ src/App.js
@@ -5,12 +5,26 @@ import TOC from './components/TOC'
 import Content from './components/Content'
 
 class App extends Component {
+  constructor (props) {
+    super(props)
+    this.state = {
+      subject: {title: 'WEB', sub: 'World wide web'},
+      contents: [
+        {id: 1, title: 'HTML', desc: 'HTML is for information'},
+        {id: 2, title: 'CSS', desc: 'CSS is for design'},
+        {id: 3, title: 'JavaScript', desc: 'JavaScript is for interaction'}
+      ]
+    }
+  }
   render () {
     return (
       <div className="App">
-        <Subject title="WEB" sub="World wide web"></Subject>
+        <Subject
+          title={this.state.subject.title}
+          sub={this.state.subject.sub}>
+        </Subject>
         <Subject title="React" sub="For UI"></Subject>
-        <TOC></TOC>
+        <TOC data={this.state.contents}></TOC>
         <Content title="HTML" dsc="HTML is HyperText Markup Language."></Content>
         <Content title="Yasuo" dsc="Yasuo is science."></Content>
       </div>

f78b584c01eae315bb513fd89cd55615ffef9fdd
kumankoishi.py <7iggif@naver.com>
2021-01-21 20:39:50 +0900

    Adding Form submission

--- src/components/CreateContent.js
+++ src/components/CreateContent.js
@@ -5,7 +5,15 @@ class CreateContent extends Component {
     return (
       <article>
         <h2>Create</h2>
-        <form>
+        <form action='/create_process' method='post'
+          onSubmit={function (e) {
+            e.preventDefault()
+            alert('submit')
+          }.bind(this)}
+        >
+          <p><input type='text' name='title' placeholder='title'></input></p>
+          <p><textarea name='desc' placeholder='description area'></textarea></p>
+          <p><input type='submit'></input></p>
         </form>
       </article>
     )
